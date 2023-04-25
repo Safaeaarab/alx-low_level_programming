07ac8cae9fe0860351767bfab95febaa3e854350
safaeaarab <safaeaarab7@gmail.com>
2023-04-25 16:18:42 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - write alphabets
+ * code that prints the alphabets
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+char ch = 'a';
+while (ch <= 'z')
+{
+_putchar(ch);
+ch++;
+}
+_putchar('\n');
+}
