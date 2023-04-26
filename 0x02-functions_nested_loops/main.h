9c149b661b647609d29a27326ae5b917b75f84d9
Defalt Astra <39121090+defaltastra@users.noreply.github.com>
2023-04-26 12:17:49 +0000

    Add files via upload

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,8 @@
-#ifndef main.h
-#define main.h
+#ifndef MAIN_H
+#define MAIN_H
 
-int _putchar(char);
+/* putchar function prototype */
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -13,5 +14,5 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-
-#endif
+void print_times_table(int n);
+#endif /* MAIN_H */
